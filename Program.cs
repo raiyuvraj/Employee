76c59dd990e15b315550442d73420436b7de4d2c
raiyuvraj <yuvirai017@gmail.com>
2022-12-02 16:57:17 -0800

    UC2_EmpDailyWage

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Emplyee is Presemt/Absent");
 Console.WriteLine("Emloyee Attendence");
+Console.WriteLine("Emplyee Daily Wedge");
